8d2061ae4fc8791f80f87f58dc4f3d8e3883eaf8
DESKTOP-PR8N81S\helLO <vairang.p@rushkar.com>
2025-11-19 11:48:57 +0530

    Code

---
+++ Github_Project/src/My_Github_Project/Practice_github_project.java
@@ -0,0 +1,19 @@
+package My_Github_Project;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class Practice_github_project {
+  @Test
+  public void f() {
+  }
+  @BeforeClass
+  public void beforeClass() {
+  }
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+}
