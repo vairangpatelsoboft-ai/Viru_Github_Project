075bf19596a9962cc8553af2ad20ab3be11755b7
DESKTOP-PR8N81S\helLO <vairang.p@rushkar.com>
2025-11-19 12:31:29 +0530

    Third New Code

---
+++ Third_Github_Project/src/Third_Github_Practice/Third_Github_Practice.java
@@ -0,0 +1,19 @@
+package Third_Github_Practice;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class Third_Github_Practice {
+  @Test
+  public void f() {
+  }
+  @BeforeClass
+  public void beforeClass() {
+  }
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+}
